f5f43d52771b1b35f2097bb9ff23bfe9c6097e80
Ionică Bizău <bizauionica@gmail.com>
2019-04-29 17:53:18 +0300

    :arrow_up: 1.0.1 :tada:

--- example/index.js
+++ example/index.js
@@ -1,11 +1,11 @@
 "use strict";
 
-const hebrewDate = require("..");
+var hebrewDate = require("..");
 
 // When not providing a date object, the months are one-indexed
 console.log(hebrewDate(2016, 10, 2));
 // { year: 5776, month: 13, date: 29, month_name: 'Elul' }
 
-let october = 9;
+var october = 9;
 console.log(hebrewDate(new Date(2016, october, 3)));
 // { year: 5777, month: 1, date: 1, month_name: 'Tishri' }
